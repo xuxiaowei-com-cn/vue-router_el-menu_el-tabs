d0169a30bfdb93498a8d1e108cb4f9a43a8dab97
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-07-22 15:54:24 +0800

    :sparkles: 优化代码，重新进入页面，移除 keep-alive 排除时的页面（组件）名动态获取，防止写错、重命名等情况

--- src/views/ConsoleView.vue
+++ src/views/ConsoleView.vue
@@ -3,14 +3,25 @@
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
 import { useStore } from '../store'
+
+const router = useRouter()
 
 const store = useStore()
 
 console.log('ConsoleView.vue')
 
-// 重新进入页面，移除 keep-alive 排除
-store.removeKeepAliveExclude('ConsoleView')
+router.isReady().then(() => {
+  // @ts-ignore
+  const matched = router.currentRoute._value.matched
+  for (const i in matched) {
+    if (matched[i].path === router.currentRoute.value.path) {
+      // 重新进入页面，移除 keep-alive 排除
+      store.removeKeepAliveExclude(matched[i].components.default.__name)
+    }
+  }
+})
 
 </script>
 
