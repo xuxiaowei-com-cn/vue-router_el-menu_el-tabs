3518f93d1308646adcfc660b9bf8e6669f8d8646
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-08-11 09:42:18 +0800

    :bug: 使用 Tooltip 文字提示，给一级菜单折叠时，增加鼠标覆盖提示语

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -19,9 +19,11 @@
 
             <!-- 无二级菜单，或二级菜单的个数小于等于 1 -->
             <el-menu-item :index="menuItemPath(item)" :key="menuItemPath(item)" v-if="childrenLength(item.children) <= 1" @click="menuItem">
-              <el-icon v-if="item.meta?.icon">
-                <component :is="item.meta?.icon"/>
-              </el-icon>
+              <el-tooltip class="box-item" effect="dark" :disabled="!isCollapse" :content="item.name" placement="right">
+                <el-icon v-if="item.meta?.icon">
+                  <component :is="item.meta?.icon"/>
+                </el-icon>
+              </el-tooltip>
               <span>{{item.name}}</span>
             </el-menu-item>
           </template>
